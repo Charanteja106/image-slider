5d3e54675e17822b9119903bd10adc872ca6706b
Charanteja106 <panchagiricharanteja@gmail.com>
2024-10-19 20:40:42 +0530

    image-sliding

---
+++ image/script.js
@@ -0,0 +1,31 @@
+let slides = document.querySelectorAll(".images")
+let container = document.querySelector(".image-container")
+
+var counter = 0;
+
+slides.forEach(
+    (slide , index) =>{
+        slide.style.left=`${index *100}%`
+
+    }
+)
+
+const goPrev = () => {
+    counter--
+    slideImage()
+}
+
+const goNext =() => {
+    counter++
+    slideImage()
+}
+
+const slideImage = () =>{
+    slides.forEach(
+        (slide) => {
+            slide.style.transform = `translateX(-${counter * 100}%)`
+        }
+    )
+}
+
+
